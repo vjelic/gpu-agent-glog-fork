af454c3493d53645f58097d137709dbebb48d358
Nitish Bhat <bhatnitish@gmail.com>
2025-03-18 11:50:52 -0700

    Update License to Apache Version 2.0 (#8)
    Signed-off-by: Nitish Bhat <nbhat@amd.com>

--- sw/nic/sdk/lib/logger/logger.c
+++ sw/nic/sdk/lib/logger/logger.c
@@ -1,19 +1,18 @@
-//
-// Copyright(C) Advanced Micro Devices, Inc. All rights reserved.
-//
-// You may not use this software and documentation (if any) (collectively,
-// the "Materials") except in compliance with the terms and conditions of
-// the Software License Agreement included with the Materials or otherwise as
-// set forth in writing and signed by you and an authorized signatory of AMD.
-// If you do not have a copy of the Software License Agreement, contact your
-// AMD representative for a copy.
-//
-// You agree that you will not reverse engineer or decompile the Materials,
-// in whole or in part, except as allowed by applicable law.
-//
-// THE MATERIALS ARE DISTRIBUTED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OR
-// REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
-//
+/*
+Copyright (c) Advanced Micro Devices, Inc. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 //------------------------------------------------------------------------------
 //
 // logger library for SDK
